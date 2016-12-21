6229639b39c3b1dc6d7bec6e2d69cd3a1b7ae3f8
Simon Let <simon.a.let@gmail.com>
2016-12-21 14:56:01 +0100

    Use docstrings instead of comments, minor improvements

--- xmlConfigParser.py
+++ xmlConfigParser.py
@@ -6,8 +6,23 @@ class XmlConfigParserException(Exception):
 
 
 class XmlConfigParser():
-    # saves default cfgValues and validates xmlFiles
+    """Parser for XML config files with defined structure
+
+    Attributes:
+        cfg (dict of dicts): expected cetegories with expected keys and optionally default values
+        root: xmlFile root 
+        defaultCategory (string): default category to use when category is omitted during various calls
+
+    """
     def __init__(self, cfgValues, xmlFile, cat=None):
+        """Constructor - saves expected keys and default values, loads and validates xmlFile, optionally sets default category
+
+	Args:
+	    cfgValues (dict of dicts): expected cetegories with expected keys and optionally default values
+	    xmlFile: path to XML config file - file is loaded and validated
+            cat (string): default category to use when category is omitted during various calls
+
+	"""
         self.cfg = cfgValues
         self.root = xml.etree.ElementTree.parse(xmlFile).getroot() 
         self.defaultCategory = cat
@@ -16,14 +31,25 @@ class XmlConfigParser():
             raise XmlConfigParserException("Xml is malformed - can't find root")
 
 
-    # @brief iterates trough cfgValues and looks for apropriate keys in xmlFile
-    # treats already present values as defaults and overrides them
-    # @return number of parsed keys - might be useful for someone
     def parseXmlConfig(self, verbious=False):
+        """Main parsing method - extracts keys specified in cfgValues from xmlFile and overwrites default values in cfgValues.
+           
+	Args:
+	    cfgValues (dict of dicts): expected cetegories with expected keys and optionally default values
+	    xmlFile (string): path to XML config file - file is loaded and validated
+            cat (string): default category to use when category is omitted during various calls
+
+	Returns:
+	    Number of read keys from xmlFile
+
+        Raises:
+            XmlConfigParserException: on missing key
+
+	"""
         count = 0
         for section, cfg in self.cfg.items():
             if verbious:
-                print("CAT: {0} config:".format(section))
+                print("CATEGORY: {0} config:".format(section))
             cfgXmlNode = self.root.find(section)
 
             for key, none in cfg.items():
@@ -50,49 +76,72 @@ class XmlConfigParser():
         return count
 
 
-    # get BOOL value from config
-    def getBool(self, key, category=None):
-        if category is None:
-            category = self.defaultCategory
-        return True if (self.cfg[category][key] != "False") else False
-
-
-    # get INT value from config
-    def getInt(self, key, category=None):
-        if category is None:
-            category = self.defaultCategory
-        return int(self.cfg[category][key])
-
-
-    # get (STRING) value from config
-    # method is called get() not getString() because no conversion is applied 
     def get(self, key, category=None):
+        """default getter method to get value for key from config
+
+        Args:
+            key (string): key to find in config
+            category (string): category to look for key in, when unsfecified use self.defaultCategory
+
+        Returns:
+            (string) value from config
+        
+        Raises:
+            XmlConfigParserException: when both argument category and default category are unspecified
+            KeyError: when category or key was not found
+            
+        """
         if category is None:
             category = self.defaultCategory
+
+        if category is None:
+            raise XmlConfigParserException("Both argument category and default category are unspecified!")
+
         return self.cfg[category][key]
     
 
-    # set (STRING) value to config
-    # use with caution
+    def getBool(self, key, category=None):
+        """get value for key from config and convert to BOOL - see get() for more details"""
+        return True if (self.get(key, category) != "False") else False
+
+
+    def getInt(self, key, category=None):
+        """get value for key from config and convert to INT - see get() for more details"""
+        return int(self.get(key, category))
+    
+
     def set(self, value, key, category=None):
+        """default setter method to set value for key in config
+
+        Args:
+            value (string): value to set
+            key (string): key to find in config
+            category (string): category to look for key in, when unsfecified use self.defaultCategory
+
+        Raises:
+            XmlConfigParserException: when both argument category and default category are unspecified
+            KeyError: when category was not found
+            
+        """
         if category is None:
             category = self.defaultCategory
+
+        if category is None:
+            raise XmlConfigParserException("Both argument category and default category are unspecified!")
+
         self.cfg[category][key] = value
     
 
-    # returns whole cfg category - useful to pass multiple values to function
     def getCategory(self, category):
+        """get whole category of keys and values as dict - useful to pass multiple values to function"""
         return self.cfg[category]
     
 
-    # returns whole cfg dict
     def getCfgDict(self):
+        """get whole config of categories as dict of dicts"""
         return self.cfg
 
 
-    # returns xml root of xmlFile structure
-    # use with caution 
     def getRoot(self):
+        """get root of xml config file"""
         return self.root
-
-
